35da8b1858ebbd13476d64b308977f42fc9c3b57
Raghu Betina <raghu@firstdraft.com>
2018-03-14 00:35:38 +0000

    Direct association between users and coachings

--- app/models/coaching.rb
+++ app/models/coaching.rb
@@ -1,6 +1,9 @@
 class Coaching < ApplicationRecord
   # Direct associations
 
+  belongs_to :coach,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :coachings,
+             :foreign_key => "coach_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
