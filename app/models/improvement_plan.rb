f336042c8f85bc2e6462fb328ddf6d5f626d6764
Raghu Betina <raghu@firstdraft.com>
2018-03-14 00:35:38 +0000

    Validate uniqueness of improvement_plans.title

--- app/models/improvement_plan.rb
+++ app/models/improvement_plan.rb
@@ -5,6 +5,8 @@ class ImprovementPlan < ApplicationRecord
 
   # Validations
 
+  validates :title, :uniqueness => { :scope => [:user_id] }
+
   validates :title, :presence => true
 
   validates :user_id, :presence => true
